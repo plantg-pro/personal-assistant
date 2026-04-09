764d3c81a1ff406a477ff40818be2c2dbca0edd2
Ivy Tang <ivivyova@gmail.com>
2026-04-09 21:36:09 +1000

    disable broken middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
-import { updateSession } from "./lib/supabase/middleware";
-import { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+export function middleware(_request: NextRequest) {
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: [],
+};
